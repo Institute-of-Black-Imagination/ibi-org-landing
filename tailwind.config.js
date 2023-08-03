d2849698ea66e0c7dc8a0a4396b7a7ce6c442e1f
Taichi Aritomo <taichiaritomo@gmail.com>
2023-08-02 22:53:28 -0400

    First commit - landing page

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    spacing: {
+      '1': '8px',
+      '2': '12px',
+      '3': '16px',
+      '4': '24px',
+      '5': '32px',
+      '6': '48px',
+      '8': '64px',
+      '10': '80px',
+      '12': '96px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
